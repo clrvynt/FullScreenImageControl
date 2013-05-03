b5df631bffe934e18097f73ecf56bed3405fc58c
kal <kalyan.krishnamurthi@biascorp.com>
2013-05-03 12:10:41 -0500

    Initial Commit

---
+++ FullScreenImageControl/KKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KKAppDelegate.h
+//  FullScreenImageControl
+//
+//  Created by kalyankrishnamurthi on 5/3/13.
+//  Copyright (c) 2013 clrvynt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FullScreenImageControl/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FullScreenImageControl
+//
+//  Created by kalyankrishnamurthi on 5/3/13.
+//  Copyright (c) 2013 clrvynt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KKAppDelegate class]));
+    }
+}
